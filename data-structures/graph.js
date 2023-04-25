12d12303e7abf08b6fd7abcea2366b7a24d574fc
Abdurahim <nurmatov.abdurahim98@mail.ru>
2023-04-25 21:31:07 +0500

    Graph traversal

--- data-structures/graph.js
+++ data-structures/graph.js
@@ -30,6 +30,66 @@ class Graph {
     }
     delete this.adjacencyList[vertex];
   }
+
+  depthFirstRecursive(startVertex) {
+    const results = [];
+    const visited = {};
+
+    const dfs = (vertex) => {
+      if (!vertex) return null;
+      results.push(vertex);
+      visited[vertex] = true;
+      this.adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) return dfs(neighbor);
+      });
+    };
+
+    dfs(startVertex);
+
+    return results;
+  }
+
+  depthFirstIterative(startVertex) {
+    const stack = [startVertex];
+    const result = [];
+    const visited = {};
+    visited[startVertex] = true;
+
+    while (stack.length) {
+      let currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+
+    return result;
+  }
+
+  breadthFirstSearch(startVertex) {
+    const queue = [startVertex];
+    const results = [];
+    const visited = {};
+    visited[startVertex] = true;
+    let currentVertex;
+
+    while (queue.length) {
+      currentVertex = queue.shift();
+      results.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          queue.push(neighbor);
+        }
+      });
+    }
+    return results;
+  }
 }
 
 const locations = new Graph();
@@ -37,15 +97,20 @@ const locations = new Graph();
 locations.addVertex("A");
 locations.addVertex("B");
 locations.addVertex("C");
+locations.addVertex("D");
+locations.addVertex("E");
+locations.addVertex("F");
 
 locations.addEdge("A", "B");
 locations.addEdge("A", "C");
-locations.addEdge("B", "C");
+locations.addEdge("B", "D");
+locations.addEdge("C", "E");
+locations.addEdge("D", "E");
+locations.addEdge("D", "F");
+locations.addEdge("E", "F");
 
-console.log("🚀 ~ file: graph.js:36 ~ locations:", locations.adjacencyList);
-
-locations.removeEdge("A", "C");
-console.log("🚀 ~ file: graph.js:12 ~ locations:", locations.adjacencyList);
-locations.removeVertex("A");
+console.log(locations.depthFirstRecursive("A"));
+console.log(locations.depthFirstIterative("A"));
+console.log(locations.breadthFirstSearch("A"));
 
 console.log("🚀 ~ file: graph.js:12 ~ locations:", locations.adjacencyList);
